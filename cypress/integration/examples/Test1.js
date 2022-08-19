cef5477333d05b7491875bab51d48dcbfa8e7bc9
pavels-s <pavels.sarkels@gmail.com>
2022-08-19 16:08:03 +0300

    Basic Assertions, hadling visible elements

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,14 @@
+/// <reference types="Cypress" />
+
 describe('My first Test Suite', function() {
 
     it('My first Test Case', function() {
         
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
+
     })
 })

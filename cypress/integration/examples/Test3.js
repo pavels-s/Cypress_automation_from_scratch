e6779fd08070d4cf06f24b7fb705b6ecfc182929
pavels-s <pavels.sarkels@gmail.com>
2022-08-23 09:16:11 +0300

    Checkbox handling

---
+++ cypress/integration/examples/Test3.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe('My second Test Suite', function() {
+
+    it('My updated Test Case', function() {
+        
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option2','option3'])
+
+    })
+    
+})

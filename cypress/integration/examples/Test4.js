a1a0ec7508a1344fbaa90f5f8c7284cce18f0ef6
pavels-s <pavels.sarkels@gmail.com>
2022-08-23 13:09:41 +0300

    Confirm and alert handling

---
+++ cypress/integration/examples/Test4.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('My second Test Suite', function() {
+
+    it('My fresh Test Case', function() {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        cy.get('#alertbtn').click()
+        cy.get('[value="Confirm"]').click()
+
+        //window:alert (Cypress automaticly closes the alerts and pop-ups)
+        cy.on('window:alert',(str) => {
+            //Mocha
+            expect(str).to.equal('Hello , share this practice page and share your knowledge')
+        })
+
+        cy.on('window:confirm',(str) => {
+            //Mocha
+            expect(str).to.equal('Hello , Are you sure you want to confirm?')
+        })
+
+    })
+
+})

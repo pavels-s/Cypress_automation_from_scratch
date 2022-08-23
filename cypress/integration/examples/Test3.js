ee786c1a41a585b235a5106516e32814087d742a
pavels-s <pavels.sarkels@gmail.com>
2022-08-23 10:28:08 +0300

    Dropdown hadling (static and dynamic)

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -6,10 +6,24 @@ describe('My second Test Suite', function() {
         
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
+        //Checkbox hadling
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check(['option2','option3'])
 
+        //Static dropdown
+        cy.get('select').select('option2').should('have.value','option2')
+
+        //Dynamic dropdown
+        cy.get('#autocomplete').type('ind')
+        cy.get('.ui-menu-item div').each(($el, index, $list) => {
+
+            if($el.text()==="India") {
+                cy.wrap($el).click()
+            }
+        })
+
+        cy.get('#autocomplete').should('have.value','India')
     })
     
 })

d34790a9ae1470a0c2f24e6de1906c846cfdedfa
pavels-s <pavels.sarkels@gmail.com>
2022-08-23 14:50:41 +0300

    Web tables with .each command

---
+++ cypress/integration/examples/Test5.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe('My new Test Suite', function() {
+
+    it('My new Test Case', function() {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        //nth-child(2) - to find 2nd child element
+        cy.get('tr td:nth-child(2)').each(($el, index, $list) => {
+
+            const text=$el.text()
+            if (text.includes("Python")) {
+
+                //Getting next sibling and text from another column.
+                //text() is a jquerry element, so we need to use it through function
+                cy.get('tr td:nth-child(2)').eq(index).next().then(function(price) {
+                    const priceText = price.text()
+                    expect(priceText).to.equal('25')
+                })
+            }
+            
+        })
+
+    })
+})

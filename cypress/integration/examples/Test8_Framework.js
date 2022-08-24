d3106f25d94c78a2d0a891e9a2c4d4186458774b
pavels-s <pavels.sarkels@gmail.com>
2022-08-24 10:38:38 +0300

    Fixtures handling

--- cypress/integration/examples/Test8_Framework.js
+++ cypress/integration/examples/Test8_Framework.js
@@ -5,16 +5,18 @@ describe('My new Test Suite', function() {
 
     //Runs once before all tests
     before(function() {
-        
-
+        cy.fixture('example').then(function(data) {
+            //second variable cant access outside (there are 2 different data variables), so we are initializing global variable
+            this.data = data
+        })
     })
 
     it('My new Test Case', function() { 
 
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
 
-        cy.get(':nth-child(1) > .form-control').type("Bob")
-        cy.get('select').select("Male")
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        cy.get('select').select(this.data.gender)
 
     })
 

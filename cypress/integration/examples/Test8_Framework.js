d1f46b50e85097d9d19a1586369af2b0363bb6b4
pavels-s <pavels.sarkels@gmail.com>
2022-08-25 12:53:55 +0300

    Country selecting haldling debug issues

--- cypress/integration/examples/Test8_Framework.js
+++ cypress/integration/examples/Test8_Framework.js
@@ -45,6 +45,21 @@ describe('My new Test Suite', function() {
         //Going to the cart
         productsPage.checkOutButton().click()
 
+        //Checkout to delivery choosing
+        cy.contains('Checkout').click()
+
+        //Country selecting
+        cy.get('#country').type('Latvia')
+        cy.get('.suggestions > ul > li > a').click()
+        cy.get('#checkbox2').click({force: true})
+        cy.get('input[type="submit"]').click()
+        //cy.get('.alert').should('have.text', 'Success! Thank you! Your order will be delivered in next few weeks :-).')
+        cy.get('.alert').then(function(element) {
+            const actualText = element.text()
+            //if (actualText.includes("Success!")) {
+            expect(actualText.includes("Success!")).to.be.true
+        })
+
     })
 
 })

2dacd159f041692523b099352eefcc94b5bbd881
pavels-s <pavels.sarkels@gmail.com>
2022-08-30 16:16:31 +0300

    Testing with front-end and back-end response

--- cypress/integration/examples/fakeTest.js
+++ cypress/integration/examples/fakeTest.js
@@ -21,8 +21,15 @@ describe('My Mocha test suite', function() {
         }).as('bookretrievals')
 
 cy.get("button[class='btn btn-primary']").click()
-cy.wait('@bookretrievals')
+
+cy.wait('@bookretrievals').should(({request, response})=> {
+    cy.get('tr').should('have.length', response.body.length+1)
+})
+
 cy.get('p').should('have.text', 'Oops only 1 Book available')
+
+//Length of the response array = rows of the table
+
 
     })
 
